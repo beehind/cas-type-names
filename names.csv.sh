4d1838ba34ee522b9521fbd8b1443dac0692f082
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-03-15 18:08:08 -0500

    include catalog number

--- names.csv.sh
+++ names.csv.sh
@@ -6,14 +6,11 @@
 
 preston cat hash://sha256/fd4193a633826e584205d6c190cf4314b707937f6072515edc0d69a61737e5d5\
  | preston dwc-stream\
- | jq --raw-output '.["http://rs.tdwg.org/dwc/terms/scientificName"]'\
- | sort\
- | uniq\
- | sed 's/^/\t/g'\
- | nomer replace gbif-parse\
+ | jq --raw-output '[.["http://rs.tdwg.org/dwc/terms/catalogNumber"],.["http://rs.tdwg.org/dwc/terms/scientificName"]] | @tsv'\
+ | nomer append gbif-parse\
+ | cut -f1,5\
  | nomer append discoverlife\
- | grep -v NONE\
- | cut -f2
+ | grep -v NONE
 
  
 
