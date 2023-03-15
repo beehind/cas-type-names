dc933f2444d371116e196730b2f808d7e6dae83a
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2023-03-15 17:54:52 -0500

    fix reference

--- names.csv.sh
+++ names.csv.sh
@@ -4,7 +4,7 @@
 
 #preston track "http://ipt.calacademy.org:8080/archive.do?r=type"
 
-preston ls\
+preston cat hash://sha256/fd4193a633826e584205d6c190cf4314b707937f6072515edc0d69a61737e5d5\
  | preston dwc-stream\
  | jq --raw-output '.["http://rs.tdwg.org/dwc/terms/scientificName"]'\
  | sort\
